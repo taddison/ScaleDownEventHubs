8cf6149867a678c14963b6f247d52e4bb1b82894
Timothy Addison <git@tjaddison.com>
2017-12-09 17:34:34 +0000

    Implement ScaleDown

---
+++ src/ScaleDownEventHubs/EventhubNamespace.cs
@@ -0,0 +1,18 @@
+﻿namespace ScaleDownEventHubs
+{
+    internal class EventhubNamespace
+    {
+        public EventhubNamespace(string subscriptionId, string resourceGroup, string @namespace, int capacity)
+        {
+            SubscriptionId = subscriptionId;
+            ResourceGroup = resourceGroup;
+            Namespace = @namespace;
+            Capacity = capacity;
+        }
+
+        public string SubscriptionId { get; set; }
+        public string ResourceGroup { get; set; }
+        public string Namespace { get; set; }
+        public int Capacity { get; set; }
+    }
+}
--- src/ScaleDownEventHubs/ScaleDown.cs
+++ src/ScaleDownEventHubs/ScaleDown.cs
@@ -1,15 +1,63 @@
-using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using System.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Azure.Management.EventHub;
+using Microsoft.Azure.Management.EventHub.Models;
 
 namespace ScaleDownEventHubs
 {
     public static class ScaleDown
     {
         [FunctionName("ScaleDown")]
-        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 30 1 * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.Info($"ScaleDown execution started");
+
+            log.Info("Getting credentials");
+            var creds = GetCredential();
+
+            var ns = new EventhubNamespace("<subscription id>", "<resource group>", "<namespace>", 1);
+
+            var ehClient = new EventHubManagementClient(creds)
+            {
+                SubscriptionId = ns.SubscriptionId
+            };
+
+            log.Info("Querying namespace information");
+            var nsInfo = ehClient.Namespaces.Get(ns.ResourceGroup, ns.Namespace);
+            if (nsInfo.Sku.Capacity <= ns.Capacity)
+            {
+                log.Info($"Namespace {ns.Namespace} in {ns.ResourceGroup} already below target capacity (Current:{nsInfo.Sku.Capacity} Target:{ns.Capacity})");
+                return;
+            }
+
+            var nsUpdate = new EHNamespace()
+            {
+                Sku = new Sku(nsInfo.Sku.Name, capacity: ns.Capacity)
+            };
+
+            log.Info($"Updating Namespace {ns.Namespace} in {ns.ResourceGroup} from {nsInfo.Sku.Capacity} to {nsUpdate.Sku.Capacity}");
+            ehClient.Namespaces.Update(ns.ResourceGroup, ns.Namespace, nsUpdate);
+
+            log.Info("ScaleDown execution completed");
+        }
+
+        private static TokenCredentials GetCredential()
+        {
+            var clientId = ConfigurationManager.AppSettings["ClientId"];
+            var clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+            var tenantId = ConfigurationManager.AppSettings["TenantId"];
+
+            var context = new AuthenticationContext($"https://login.microsoftonline.com/{tenantId}");
+
+            var result = context.AcquireTokenAsync(
+                "https://management.core.windows.net/",
+                new ClientCredential(clientId, clientSecret)
+            ).Result;
+
+            return new TokenCredentials(result.AccessToken);
         }
     }
 }
