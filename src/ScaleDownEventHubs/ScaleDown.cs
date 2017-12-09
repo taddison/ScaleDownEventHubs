f3dee8bd788a37013e633cf1f1a9ed495b180c04
Timothy Addison <git@tjaddison.com>
2017-12-09 17:39:58 +0000

    Support multiple namespaces

--- src/ScaleDownEventHubs/ScaleDown.cs
+++ src/ScaleDownEventHubs/ScaleDown.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Rest;
 using Microsoft.Azure.Management.EventHub;
 using Microsoft.Azure.Management.EventHub.Models;
+using System.Collections.Generic;
 
 namespace ScaleDownEventHubs
 {
@@ -16,11 +17,25 @@ namespace ScaleDownEventHubs
             log.Info($"ScaleDown execution started");
 
             log.Info("Getting credentials");
-            var creds = GetCredential();
+            var creds = GetCredentials(log);
 
-            var ns = new EventhubNamespace("<subscription id>", "<resource group>", "<namespace>", 1);
+            var namespaces = new List<EventhubNamespace>
+            {
+                new EventhubNamespace("<subscription id>", "<resource group>", "<namespace>", 1)
+            };
 
-            var ehClient = new EventHubManagementClient(creds)
+            foreach (var ns in namespaces)
+            {
+                log.Info($"Processing scaledown for {ns.Namespace} in {ns.ResourceGroup}");
+                ScaleDownNamespace(ns, creds, log);
+            }
+            
+            log.Info("ScaleDown execution completed");
+        }
+
+        private static void ScaleDownNamespace(EventhubNamespace ns, TokenCredentials credentials, TraceWriter log)
+        {
+            var ehClient = new EventHubManagementClient(credentials)
             {
                 SubscriptionId = ns.SubscriptionId
             };
@@ -40,11 +55,9 @@ namespace ScaleDownEventHubs
 
             log.Info($"Updating Namespace {ns.Namespace} in {ns.ResourceGroup} from {nsInfo.Sku.Capacity} to {nsUpdate.Sku.Capacity}");
             ehClient.Namespaces.Update(ns.ResourceGroup, ns.Namespace, nsUpdate);
-
-            log.Info("ScaleDown execution completed");
         }
 
-        private static TokenCredentials GetCredential()
+        private static TokenCredentials GetCredentials(TraceWriter log)
         {
             var clientId = ConfigurationManager.AppSettings["ClientId"];
             var clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
@@ -52,6 +65,7 @@ namespace ScaleDownEventHubs
 
             var context = new AuthenticationContext($"https://login.microsoftonline.com/{tenantId}");
 
+            log.Info("Attempting to acquire token");
             var result = context.AcquireTokenAsync(
                 "https://management.core.windows.net/",
                 new ClientCredential(clientId, clientSecret)
