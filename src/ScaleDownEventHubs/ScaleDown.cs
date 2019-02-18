dc1681d96612a1f33303316b321fd5abcff795c6
Tim Addison <git@tjaddison.com>
2019-02-18 16:11:30 -0500

    Remove old version, rename v2 to main

--- src/ScaleDownEventHubs/ScaleDown.cs
+++ src/ScaleDownEventHubs/ScaleDown.cs
@@ -1,13 +1,15 @@
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using System.Configuration;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Microsoft.Rest;
 using Microsoft.Azure.Management.EventHub;
 using Microsoft.Azure.Management.EventHub.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Rest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 using System.Text.RegularExpressions;
 
 namespace ScaleDownEventHubs
@@ -15,77 +17,98 @@ namespace ScaleDownEventHubs
     public static class ScaleDown
     {
         [FunctionName("ScaleDown")]
-        public static void Run([TimerTrigger("* */1 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
-            log.Info($"ScaleDown execution started");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-            log.Info("Getting credentials");
-            var creds = GetCredentials(log);
-        
-            var subscriptions = new List<string>()
+            var creds = GetCredentials(config, log);
+
+            var subs = GetSubscriptions(creds, log);
+
+            log.LogInformation("Enumerating authorized subscriptions");
+            foreach (var subscription in subs)
             {
-                "fcba2349-45f5-4b30-a9bb-b075cec3622f"
-            };
-            
-            foreach (var subscription in subscriptions)
-            {
-                log.Info($"Processing scaledown for {subscription}");
-                ScaleDownNamespacesInSubscription(subscription, creds, log);
+                log.LogInformation($"Processing scaledown for subId:{subscription.SubscriptionId}, Name:{subscription.DisplayName}");
+                ScaleDownNamespacesInSubscription(subscription.SubscriptionId, creds, log);
             }
-            
-            log.Info("ScaleDown execution completed");
         }
 
-        private static void ScaleDownNamespacesInSubscription(string subscriptionId, TokenCredentials credentials, TraceWriter log)
+        private static IEnumerable<ISubscription> GetSubscriptions(AzureCredentials credentials, ILogger log)
+        {
+            log.LogInformation("Authenticate as service principal");
+            var azure = Azure.Authenticate(credentials);
+
+            log.LogInformation("Get subscriptions");
+            return azure.Subscriptions.List();
+        }
+
+        private static void ScaleDownNamespacesInSubscription(string subscriptionId, ServiceClientCredentials credentials, ILogger log)
         {
             var ehClient = new EventHubManagementClient(credentials)
             {
                 SubscriptionId = subscriptionId
             };
 
-            log.Info($"Getting namespaces for {subscriptionId}");
-            var namespaces = GetNamespacesForSubscription(ehClient);
+            IEnumerable<EventhubNamespace> namespaces = null;
+            try
+            {
+                namespaces = GetNamespacesForSubscription(ehClient, log);
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, $"Error getting namespaces for subscription {subscriptionId}");
+                return;
+            }
 
             foreach (var ns in namespaces)
             {
                 try
                 {
                     ScaleDownNamespace(ns, ehClient, log);
-                } catch (Exception e)
+                }
+                catch (Exception e)
                 {
-                    log.Error("Error", e);
+                    log.LogError(e, $"Error scaling down namespace {ns.Namespace}");
                 }
             }
         }
 
-        private static IEnumerable<EventhubNamespace> GetNamespacesForSubscription(EventHubManagementClient ehClient)
+        private static IEnumerable<EventhubNamespace> GetNamespacesForSubscription(EventHubManagementClient ehClient, ILogger log)
         {
-            var nsList = new List<EventhubNamespace>();
-
+            log.LogInformation($"Getting namespaces for {ehClient.SubscriptionId}");
             var namespaces = ehClient.Namespaces.List().ToList();
 
-
-            foreach(var ns in namespaces)
+            var nsList = new List<EventhubNamespace>();
+            foreach (var ns in namespaces)
             {
+                log.LogInformation($"Processing namespace {ns.Name} to extract RG and Throughput Units");
+
                 var resourceGroupName = Regex.Match(ns.Id, @".*\/resourceGroups\/([\w-]+)\/providers.*").Groups[1].Value;
+
                 int targetThroughputUnits = 1;
                 if (ns.Tags.ContainsKey("ScaleDownTUs"))
                 {
                     int.TryParse(ns.Tags["ScaleDownTUs"], out targetThroughputUnits);
                 }
+
                 nsList.Add(new EventhubNamespace(ehClient.SubscriptionId, resourceGroupName, ns.Name, targetThroughputUnits));
             }
 
             return nsList;
         }
 
-        private static void ScaleDownNamespace(EventhubNamespace ns, EventHubManagementClient ehClient, TraceWriter log)
+        private static void ScaleDownNamespace(EventhubNamespace ns, EventHubManagementClient ehClient, ILogger log)
         {
-            log.Info("Querying namespace information");
+            log.LogInformation($"ScaleDownNamespace for ns:{ns.Namespace}");
+
             var nsInfo = ehClient.Namespaces.Get(ns.ResourceGroup, ns.Namespace);
             if (nsInfo.Sku.Capacity <= ns.TargetThroughputUnits)
             {
-                log.Info($"Namespace {ns.Namespace} in {ns.ResourceGroup} already below target capacity (Current:{nsInfo.Sku.Capacity} Target:{ns.TargetThroughputUnits})");
+                log.LogInformation($"Namespace:{ns.Namespace} in RG:{ns.ResourceGroup} already at or below target capacity (Current:{nsInfo.Sku.Capacity} Target:{ns.TargetThroughputUnits})");
                 return;
             }
 
@@ -94,25 +117,17 @@ namespace ScaleDownEventHubs
                 Sku = new Sku(nsInfo.Sku.Name, capacity: ns.TargetThroughputUnits)
             };
 
-            log.Info($"Updating Namespace {ns.Namespace} in {ns.ResourceGroup} from {nsInfo.Sku.Capacity} to {nsUpdate.Sku.Capacity}");
+            log.LogInformation($"Updating Namespace:{ns.Namespace} in RG:{ns.ResourceGroup} from:{nsInfo.Sku.Capacity} to:{nsUpdate.Sku.Capacity}");
             ehClient.Namespaces.Update(ns.ResourceGroup, ns.Namespace, nsUpdate);
         }
 
-        private static TokenCredentials GetCredentials(TraceWriter log)
+        private static AzureCredentials GetCredentials(IConfigurationRoot config, ILogger log)
         {
-            var clientId = ConfigurationManager.AppSettings["ClientId"];
-            var clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
-            var tenantId = ConfigurationManager.AppSettings["TenantId"];
+            var clientId = config["ClientId"];
+            var clientSecret = config["ClientSecret"];
+            var tenantId = config["TenantId"];
 
-            var context = new AuthenticationContext($"https://login.microsoftonline.com/{tenantId}");
-
-            log.Info("Attempting to acquire token");
-            var result = context.AcquireTokenAsync(
-                "https://management.core.windows.net/",
-                new ClientCredential(clientId, clientSecret)
-            ).Result;
-
-            return new TokenCredentials(result.AccessToken);
+            return new AzureCredentialsFactory().FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
         }
     }
 }
